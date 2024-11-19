7eaad88df61feb9040cb5535300a273a951f1940
DELL <DELL@HOME-PC>
2024-11-19 11:43:49 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication18.cpp
@@ -0,0 +1,36 @@
+﻿//Створити стек цілих чисел. Обчислити кількість парних елементів стеку.
+#include <iostream>
+#include <stack>
+#include <windows.h>
+using namespace std;
+int main()
+{
+    SetConsoleOutputCP(1251); // Установка української локалізації для виводу
+    stack<int> s;
+    int n, x, evenCount = 0; // Змінна для підрахунку парних елементів
+
+    cout << "Введіть кількість елементів: ";
+    cin >> n;
+    cout << "Введіть " << n << " елементів:\n";
+
+    // Введення елементів у стек
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        s.push(x);
+    }
+
+    // Обчислення кількості парних елементів
+    while (!s.empty()) {
+        int element = s.top();
+        s.pop();
+
+        if (element % 2 == 0) { // Перевірка на парність
+            evenCount++;
+        }
+    }
+
+    cout << "Кількість парних елементів у стеку: " << evenCount << endl;
+    return 0;
+}
+
+
